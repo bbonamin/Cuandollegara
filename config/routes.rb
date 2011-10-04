4a04b37f0b8d85b4712661e5326937a442f42965
Bruno Bonamin <bruno.bonamin@gmail.com>
2011-10-04 00:32:19 -0300

    Funcionando model de favoritos, agregar favoritos y clickearlos para ver las llegadas

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Whenbus::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :buses, :only => []
-
+  resources :favorites
+  
   get "arrival/select"
   get "arrival/show"
   get "arrival/select_line"
