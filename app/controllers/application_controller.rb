17f0af2a6b839c15523139b095a40ebe99489de5
Bruno Bonamin <bruno.bonamin@gmail.com>
2011-10-07 19:00:34 -0300

    Implementacion parcial twitter bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
   MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
 
   def detect_browser
-    agent = request.headers["HTTP_USER_AGENT"].downcase
-    MOBILE_BROWSERS.each do |m|
-      return "mobile" if agent.match(m)
-    end
-    return "application"
+    # agent = request.headers["HTTP_USER_AGENT"].downcase
+    # MOBILE_BROWSERS.each do |m|
+    #   return "mobile" if agent.match(m)
+    # end
+    # return "application"
+    return "layout_new"
   end
 end
